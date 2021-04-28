b1f602a3750a18b835b47eb53b44b1337d956f79
Elsa Portilla <elsa.portilla@gmail.com>
2021-04-28 11:59:02 -0500

    single product connect api SUCCESS

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,17 +1,22 @@
-const SingleProduct = () => {
+import getHash from '../utils/getHash';
+import getData from '../utils/getData';
+
+const SingleProduct = async () => {
+    const id = getHash();
+    const product = await getData(id);
     const view = `
         <div class="Product-inner">
             <article class="Product-card">
-                <img src="image" alt="name"/>
-                <h2>Product:</h2>
+                <img src="${product.data.api_featured_image}" alt="${product.data.name}"/>
+                <h2>${product.data.name}</h2>
             </article>
             <article class="Product-card">
-                <h3>Brand:</h3>
-                <h3>Price: $</h3>
-                <h3>Type: </h3>
-                <h3>Description:</h3>
+                <h3>Brand: ${product.data.brand}</h3>
+                <h3>Price: $${product.data.price}</h3>
+                <h3>Type: ${product.data.product_type} </h3>
+                <h3>Description: ${product.data.description}</h3>
                 <div class="boton">
-                    <a hfer="product_link">
+                    <a hfer="${product.data.product_link}k">
                         BUY
                     </a>
                 </div>
